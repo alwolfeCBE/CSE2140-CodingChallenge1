a43654d12f3735377a10ca7c03687f16c6f29543
Akhila Wolfe <akhilawolfe@Akhilas-MacBook-Air.local>
2024-09-12 17:40:52 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner myScan = new Scanner(System.in);
+        System.out.println("Emter a 4 digit year")
+        int year = myScan.nextInt();
+        /* using MATHEMATICAL operations (no Strings), split a year
+        Example: if the user enters 1989, the output would be:
+        1
+        9
+        8
+        9
+
+         */
+    }
+}
+//Comment on how you tested the code to make sure it works in all situations
+//include tests that you ran
